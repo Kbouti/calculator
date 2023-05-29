d351a6628cdf4071ecfd2460b3fad30eb4e2e925
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-05-29 11:17:04 -0700

    Cache the dom

--- javascript.js
+++ javascript.js
@@ -1,2 +1,28 @@
-console.log(`Welcome to Calulator`)
+console.log(`Welcome to Calculator`)
 
+
+const upperText_div = document.getElementById(`upperText`);
+const lowerText_div = document.getElementById(`lowerText`);
+
+
+const clearButton_button = document.getElementById(`clearButton`);
+const divideButton_button = document.getElementById(`divideButton`);
+const multiplyButton_button = document.getElementById(`multiplyButton`);
+
+const sevenButton_button = document.getElementById(`sevenButton`);
+const eightButton_button = document.getElementById(`eightButton`);
+const nineButton_button = document.getElementById(`nineButton`);
+const subtractButton_button = document.getElementById(`subtractButton`);
+
+const fourButton_button = document.getElementById(`fourButton`);
+const fiveButton_button = document.getElementById(`fiveButton`);
+const sixButton_button = document.getElementById(`sixButton`);
+const addButton_button = document.getElementById(`addButton`);
+
+const oneButton_button = document.getElementById(`oneButton`);
+const twoButton_button = document.getElementById(`twoButton`);
+const threeButton_button = document.getElementById(`threeButton`);
+const equalButton_button = document.getElementById(`equalButton`);
+
+const zeroButton_button = document.getElementById(`zeroButton`);
+const dotButton_button = document.getElementById(`dotButton`);
